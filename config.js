a9071dea1a1588857dbbe7a01d50eb641f31620d
ANDREW-ALUNO <andrew.silva@alu.apac.org.br>
2023-06-26 17:47:28 -0300

    PROJETO 71 quase acabando

--- config.js
+++ config.js
@@ -1,10 +1,17 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-var firebaseConfig = {
-  //Cole sua Configuração do Firebase aqui
+
   
+  const firebaseConfig = {
+  apiKey: "AIzaSyDzIDm61Kg3wLPdDpSDwvu9Y0rc8nj4mOQ",
+  authDomain: "ciclista-eletronico-95a1d.firebaseapp.com",
+  projectId: "ciclista-eletronico-95a1d",
+  storageBucket: "ciclista-eletronico-95a1d.appspot.com",
+  messagingSenderId: "103690703204",
+  appId: "1:103690703204:web:e1ce3ccfe99b5ca780ea71"
 };
+
 
 firebase.initializeApp(firebaseConfig);
 
